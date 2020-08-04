5e17b4b6d18238f58f954724305cdc7f743e5d83
Rajas Kakodkar <rajas.kakodkar@ibm.com>
2020-08-04 15:04:35 +0530

    Update entrypoint.sh

--- images/bootstrap/entrypoint.sh
+++ images/bootstrap/entrypoint.sh
@@ -19,15 +19,18 @@ set -o pipefail
 
 # get test-infra for latest bootstrap etc
 git clone https://github.com/kubernetes/test-infra
-curl -o job.patch https://raw.githubusercontent.com/rajaskakodkar/utility/master/job.patch
-patch test-infra/jobs/config.json job.patch
 #BOOTSTRAP_UPLOAD_BUCKET_PATH=${BOOTSTRAP_UPLOAD_BUCKET_PATH:-"gs://kubernetes-jenkins/logs"}
 
 # actually start bootstrap and the job, under the runner (which handles dind etc.)
-/usr/local/bin/runner.sh \
-    ./test-infra/jenkins/bootstrap.py \
-        --job=kubernetes \
-        --repo=k8s.io/kubernetes=v1.18.4 \
-#        --service-account="${GOOGLE_APPLICATION_CREDENTIALS}" \
-#        --upload="${BOOTSTRAP_UPLOAD_BUCKET_PATH}" \
+export JOB=${JOB:-"kubernetes-integration"}
+export REPO=${REPO:-"k8s.io/kubernetes=$(curl --silent "https://api.github.com/repos/kubernetes/kubernetes/releases/latest" | jq -r .tag_name)"}
+if [[ $JOB == "kubernetes-integration"* ]]; then
+        sed -i 's/^{/{"kubernetes-integration":{},/' test-infra/jobs/config.json
+        /usr/local/bin/runner.sh \
+                ./test-infra/jenkins/bootstrap.py \
+                --job=${JOB} \
+                --repo=${REPO} \
+                --root=/go/src \
+                --scenario=execute -- bash -- -c 'sed -i "s/--timeout=120/--timeout=300/" hack/make-rules/test.sh && sed -i "s/export KUBE_RACE/#export KUBE_RACE/" ./hack/jenkins/test-dockerized.sh && sed -i "s/WORKSPACE/PWD/" ./hack/jenkins/test-dockerized.sh && sed -i "/install-etcd/d" ./hack/jenkins/test-dockerized.sh && sed -i "s|k8s.gcr.io/serve_hostname|gcr.io/kubernetes-e2e-test-images/serve-hostname-s390x:1.2|" test/fixtures/doc-yaml/admin/limitrange/valid-pod.yaml && sed -i "s/make test-integration/make test-integration KUBE_TEST_ARGS=\"-p 1\"/" ./hack/jenkins/test-dockerized.sh && sed -i "s/exceeded/or context cancellation/" $GOPATH/src/k8s.io/kubernetes/test/cmd/request-timeout.sh && ./hack/jenkins/test-dockerized.sh' \
         "$@"
+fi
